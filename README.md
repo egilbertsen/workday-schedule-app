9a160d255f4d7c769f279bb13b2f46b08903f93d
EG5924 <67492348+egilbertsen@users.noreply.github.com>
2020-08-14 20:01:20 -0700

    added moment.js features

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ A daily schedule app using local storage to maintain saved events
 This app is a basic daily calendar, allowing the user to create and save events during the workday. This app was built using jQuery, Bootstrap, and Moment.js. 
 
 
+## Features
+
+Moment.js is used to set the date at the top of the calendar. It was also used to create color variation helping to designate the past, present, and future portions of the workday.
+
--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 $(document).ready(function(){
+    
+    currentDateDisp();
+
+    function currentDateDisp() {
+        // got a lot of help from https://devhints.io/datetime
+        var currentDateStr = moment().format('dddd, MMMM Do YYYY');
+        $('#currentDay').text(currentDateStr);
+    }
+    
     generateTimeBlocks();
     
-    function generateTimeBlocks () {
+    function generateTimeBlocks() {
         var timeContainer = $(".container");
 
         for (var i = 6; i < 18; i++) {
             var newTimeRow = $("<div>");
             newTimeRow.addClass("row time-block");
+            newTimeRow.attr("data-hour", i);
 
             var newTimeDiv = $("<div>");
             newTimeDiv.addClass("col-md-1 hour");
@@ -27,5 +37,24 @@ $(document).ready(function(){
             timeContainer.append(newTimeRow);
         }
     }
+
+    var rowArr = $(".time-block");
+
+    var currentHour = moment().format('HH');
+    var currentHourParsed = parseInt(currentHour);
+
+    rowArr.each(function() {
+        
+        var thisHour = parseInt($(this).data("hour"));
+        console.log(thisHour);
+
+        if (thisHour == currentHourParsed) {
+            $(this).addClass("present")
+        } else if (thisHour < currentHourParsed) {
+            $(this).addClass("past")
+        } else if (thisHour > currentHourParsed) {
+            $(this).addClass("future")
+        }
+    })
     
 });
